3fdcc0756d0f362d469bd0eb89ce371da88a10a1
thrisha <kotturithrishasree@gmail.com>
2024-10-19 13:47:42 +0530

    message

---
+++ test1/src/App.jsx
@@ -0,0 +1,29 @@
+import {BrowserRouter, Route, Routes,Link} from 'react-router-dom';
+import Login from './Login';
+import About from './About';
+import Signup from './Signup';
+
+function Header(){
+  return(
+    <ul>
+      <li><Link to="/">Login</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/signup">Signup</Link></li>
+    </ul>
+  )
+}
+function App(){
+  return(
+    <div>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+    <Route path="/" element={<Login/>}/>
+    <Route path="/signup" element={<Signup/>}/>
+    <Route path="/about" element={<About/>}/>
+    </Routes>
+    </BrowserRouter>
+    </div>
+  )
+}
+export default App;
